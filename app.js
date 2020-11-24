bbe9117b3e1e45deb3279802be3453cabab90875
Fred Liu <liuxiaokun19890410@hotmail.com>
2020-11-24 08:15:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //app.js
 App({
   globalData: {
-    appid: 'wx8b1425f477a34c5c',
-    secret: 'fdff1b8f98212d5eb32a076fa8e06ad4',
+    appid: 'wx8b1425f3477a34c5c',
+    secret: 'fdff1b8f982412d5eb32a076fa8e06ad4',
   },
   onLaunch: function () {
     let _this = this
